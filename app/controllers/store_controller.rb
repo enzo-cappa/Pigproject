23a47406c88eee93bd87e5d4366ec14269b99ee9
Enzo Cappa <zherk@dKdm.intra.sitrack.com>
2009-12-04 11:32:51 -0300

    Arreglo los permisos para la compra de productos

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,6 +1,5 @@
 class StoreController < ApplicationController
   before_filter :find_cart, :except => :empty_cart
-  before_filter :authorize_as_user, :except => :index
 
   def index
     @letter = params[:letter].blank? ? 'a' : params[:letter]
@@ -38,25 +37,23 @@ class StoreController < ApplicationController
   end
 
   def save_order
-    @cart = find_cart
-    @order = Order.new(params[:order])
-    @order.add_line_items_from_cart(@cart)
-    if @order.save
-      session[:cart] = nil
-      redirect_to_index(I18n.t('flash.thanks' ))
+    if User.find_by_id(session[:user_id])
+      @cart = find_cart
+      @order = Order.new(params[:order])
+      @order.add_line_items_from_cart(@cart)
+      if @order.save
+        session[:cart] = nil
+        redirect_to_index(I18n.t('flash.thanks' ))
+      else
+        render :action => 'checkout'
+      end
     else
-      render :action => 'checkout'
-    end
-  end
-
-  protected
-
-  def authorize_as_user
-    unless User.find_by_id(session[:user_id])
       flash[:notice] = "Please log in"
       redirect_to :controller => 'admin' , :action => 'login'
     end
   end
+
+  protected
 
   def authorize
   end
