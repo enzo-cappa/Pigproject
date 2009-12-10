b8566cc91db4c0a3b05169986d180bb67308772b
Enzo Cappa <zherk@dKdm.intra.sitrack.com>
2009-12-10 11:12:51 -0300

    añado el monto por usuario teniendo en cuenta rpedidos y depositos

--- app/controllers/deposits_controller.rb
+++ app/controllers/deposits_controller.rb
@@ -40,7 +40,22 @@ class DepositsController < ApplicationController
   end
 
   def amount_by_user
-    @users_amount_map = User.find(:all).map{ |u| [u.name, u.deposits.sum(:amount)]}
+
+    @users_amount_map = { }
+
+    User.find(:all).each do |u|
+      deposits = u.deposits.sum(:amount)
+      orders = 0
+      u.orders.each do |o|
+        o.line_items.each do |l|
+          orders += l.quantity * l.product.price
+        end
+      end
+      total = deposits - orders
+      @users_amount_map [u.name] = total
+    end
+
+
   end
 
 end
