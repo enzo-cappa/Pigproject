757773a275c77462ebaff542e4c000ce8f42c078
Enzo <zherk@dKdm.com>
2010-04-12 12:04:20 -0300

    ahora si las noticias

---
+++ app/controllers/news_controller.rb
@@ -0,0 +1,19 @@
+require 'rss/2.0'
+require 'open-uri'
+
+class NewsController < ApplicationController
+  def index
+    feed_url = 'http://www.pagina12.com.ar/diario/rss/ultimas_noticias.xml'
+    output = "<h1>Noticias</h1>" 
+    open(feed_url) do |http|
+      response = http.read
+      @rss = RSS::Parser.parse(response, false)
+    end
+  end
+
+  protected
+
+  def authorize
+  end
+
+end
