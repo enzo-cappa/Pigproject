24001042e04d907bf8480a07a5ffa43280eaf2af
Enzo <zherk@dKdm.com>
2010-04-12 14:53:35 -0300

    agrego noticias de linux

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -2,8 +2,19 @@ require 'rss/2.0'
 require 'open-uri'
 
 class NewsController < ApplicationController
+  
+  LINUX_SOURCE='http://lxer.com/module/newswire/headlines.rdf'
+  GENERAL_SOURCE='http://www.pagina12.com.ar/diario/rss/ultimas_noticias.xml'
+  
   def index
-    feed_url = 'http://www.pagina12.com.ar/diario/rss/ultimas_noticias.xml'
+    
+    feed_url=''
+    if params[:source] == 'linux'
+      feed_url = LINUX_SOURCE
+    else
+      feed_url = GENERAL_SOURCE
+    end
+    
     output = "<h1>Noticias</h1>" 
     open(feed_url, :proxy => 'http://172.16.102.1:3128') do |http|
       response = http.read
