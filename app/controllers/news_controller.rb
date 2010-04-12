c1ba01fdd1ecb67529ee3e16237687a06dea41cc
Enzo <zherk@dKdm.com>
2010-04-12 12:17:47 -0300

    proxy

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -5,7 +5,7 @@ class NewsController < ApplicationController
   def index
     feed_url = 'http://www.pagina12.com.ar/diario/rss/ultimas_noticias.xml'
     output = "<h1>Noticias</h1>" 
-    open(feed_url) do |http|
+    open(feed_url, :proxy => 'http://172.16.102.1:3128') do |http|
       response = http.read
       @rss = RSS::Parser.parse(response, false)
     end
